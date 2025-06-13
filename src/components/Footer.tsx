889492c501ab1d0a1d57863812e19cf3ca23d7a5
freshlab-web <almir@freshlab.com.br>
2025-06-13 19:11:20 +0000

    troca  tel: do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -73,7 +73,7 @@ export default function Footer() {
               </li>
               <li className="flex items-start">
                 <Phone size={20} className="mr-3 mt-1 text-talkaio-blue" />
-                <a href="tel:+5511999999999" className="text-gray-300 hover:text-talkaio-blue transition-colors">
+                <a href="tel:+5511918257335" className="text-gray-300 hover:text-talkaio-blue transition-colors">
                   (11) 91825-7335
                 </a>
               </li>
